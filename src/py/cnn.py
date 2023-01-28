6d647b2a1caa2f6b66d0930fd892e6dcded5f752
Antonio León Villares <55582768+alv31415@users.noreply.github.com>
2023-01-28 19:25:42 +0000

    Fixed input dimension error in cnn

--- src/py/cnn.py
+++ src/py/cnn.py
@@ -1,5 +1,5 @@
+import torch
 import torch.nn as nn
-from mlp import MLP
 
 class CNNCell(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, use_batch_norm = True, use_pool = True):
@@ -22,19 +22,26 @@ class CNN(nn.Module):
     def __init__(self, input_dim, in_channels, output_dim, use_bias = True, use_batch_norm = True):
         super(CNN, self).__init__()
 
+        lin_in_features = int((input_dim / 2 ** 5)**2 * 256)
+        print(lin_in_features)
+
         # define the model
         self.cnn = nn.Sequential(
                     CNNCell(in_channels = in_channels, out_channels = 32, kernel_size = 3, stride = 1, padding = 1,
-                             use_batch_norm = use_batch_norm, use_pool = False),
+                             use_batch_norm = use_batch_norm, use_pool = True),
                     CNNCell(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1,
                             use_batch_norm=use_batch_norm, use_pool=True),
                     CNNCell(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1,
-                            use_batch_norm=use_batch_norm, use_pool=False),
+                            use_batch_norm=use_batch_norm, use_pool=True),
+                    CNNCell(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1,
+                            use_batch_norm=use_batch_norm, use_pool=True),
                     CNNCell(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1,
                             use_batch_norm=use_batch_norm, use_pool=True),
                     nn.Flatten(),
-                    nn.Linear(in_features = (input_dim / 4 ** 2)**2 * 256, out_features = output_dim, bias = use_bias)
+                    nn.Linear(in_features = lin_in_features, out_features = output_dim, bias = use_bias)
                 )
 
+    @torch.no_grad()
     def forward(self, x):
-        return self.cnn(x)
+        f = torch.max(x)
+        return self.cnn(x/f)
