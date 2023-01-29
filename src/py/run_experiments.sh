09b695e46c2ce888ce4bb4c2e2987bb2cda5037c
Antonio León Villares <55582768+alv31415@users.noreply.github.com>
2023-01-29 17:48:15 +0000

    Fix experiment runner - missing path to experiments.txt

--- src/py/run_experiments.sh
+++ src/py/run_experiments.sh
@@ -1,3 +1,6 @@
+# make script bail out after first error
+set -e
+
 # gather the files & directories to use
 STUDENT_ID=$(whoami)
 EXPERIMENT_DIR=/home/${STUDENT_ID}/honours-project/contrastive-map/src/py
@@ -10,12 +13,15 @@ SCRATCH_OUT_DIR=${SCRATCH_DIR}/output
 MAIN_FILE=${EXPERIMENT_DIR}/main.py 
 SLURM_RUN_FILE=${EXPERIMENT_DIR}/batch_slurm_contrastive_run.sh
 GEN_EXPERIMENT_FILE=${EXPERIMENT_DIR}/generate_experiments.py
+EXPERIMENT_FILE=${EXPERIMENT_DIR}/experiments.txt
 
 # generate experiments file
 echo "Generating experiments.txt"
 python ${GEN_EXPERIMENT_FILE} --main ${MAIN_FILE} \
 							  --scratch-out-dir ${SCRATCH_OUT_DIR} \
 							  --scratch-data-dir ${SCRATCH_DATA_DIR}
+
+echo "experiments.txt created succesfully"
 
 
 N_EXPERIMENTS=$(cat ${EXPERIMENT_FILE} | wc -l) 
