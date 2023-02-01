d2b03b0b46be9740fb9e3e52521a4f345432e4e4
Antonio León Villares <55582768+alv31415@users.noreply.github.com>
2023-02-01 13:24:18 +0000

    The dataset now removes the blank patches which are removed from the dataset

--- src/py/patch_dataset.py
+++ src/py/patch_dataset.py
@@ -17,9 +17,10 @@ from torch.utils.data import Dataset
 from map_patch import MapPatch
 
 class CLPatchDataset(Dataset):
-    def __init__(self, X_1, X_2):
+    def __init__(self, X_1, X_2, removed_patches = None):
         self.X_1 = X_1
         self.X_2 = X_2
+        self.removed_patches = []
 
     def __len__(self):
         return len(self.X_1)
@@ -35,16 +36,20 @@ class CLPatchDataset(Dataset):
 
         return (self.X_1[i].patch, self.X_2[i].patch)
 
+    def get_removed_patch(self, i):
+        return self.removed_patches[i]
+
     @classmethod
     def from_dir(cls, map_directory, patch_width, verbose=False):
         X_1 = []
         X_2 = []
+        removed_patches = []
 
         logging.info(f"Directories found: {sorted(os.listdir(map_directory))}")
         logging.info(
             f"Files in first directory: {os.listdir(os.path.join(map_directory, os.listdir(map_directory)[0]))}")
 
-        total_removed_patches = 0
+
         for folder in os.listdir(map_directory):
             if folder.isdigit():
                 if verbose:
@@ -52,14 +57,15 @@ class CLPatchDataset(Dataset):
 
                 directory = os.path.join(map_directory, folder)
                 patch_list = CLPatchDataset.get_patch_list_from_dir(directory, patch_width=patch_width, verbose=False)
-                x_1, x_2, n_removed_patches = CLPatchDataset.get_matching_patch_list(patch_list)
+                x_1, x_2, _removed_patches = CLPatchDataset.get_matching_patch_list(patch_list)
                 X_1.extend(x_1)
                 X_2.extend(x_2)
-                total_removed_patches += n_removed_patches
+                removed_patches.extend(_removed_patches)
 
-        logging.info(f"Generated {len(X_1)} positive pairs, after removing {total_removed_patches} positive pairs.")
 
-        return cls(X_1, X_2)
+        logging.info(f"Generated {len(X_1)} positive pairs, after removing {len(removed_patches)} positive pairs.")
+
+        return cls(X_1, X_2, removed_patches)
 
     @staticmethod
     def index_sampler(indices, n_samples=4):
@@ -104,11 +110,11 @@ class CLPatchDataset(Dataset):
 
         sample_indices = [(k, j) for k in range(n_samples) for j in range(k) if k != j]
         n_patches = set([len(patch_list[i]) for i in range(n_samples)])
-        n_removed_patches = 0
+        removed_patches = []
 
         if len(n_patches) != 1:
             logging.error(f"Found different patch list lengths: {n_patches}. Patches won't be added to the dataset.")
-            return x_1, x_2, n_removed_patches
+            return x_1, x_2, len(removed_patches)
 
         for i in range(len(patch_list[0])):
             for index in sample_indices:
@@ -120,13 +126,18 @@ class CLPatchDataset(Dataset):
                         x_1.append(patch_1)
                         x_2.append(patch_2)
                     else:
-                        n_removed_patches += 1
+                        if x_1 not in removed_patches:
+                            removed_patches.append(x_1)
+
+                        if x_2 not in removed_patches:
+                            removed_patches.append(x_2)
+
                 except IndexError:
                     logging.error(
                         f"Error processing patches. Tried fetching patches at index {i}, but patch lists have lengths {len(patch_list[index[0]])} and {len(patch_list[index[1]])}")
                     raise ValueError()
 
-        return x_1, x_2, n_removed_patches
+        return x_1, x_2, removed_patches
 
     def save(self, file_name):
         with open(file_name, "wb") as f:
