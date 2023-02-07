ae2d29e2d239da99f2afd129bee64019c3fe4466
alv31415 <aleonvillares31415@gmail.com>
2023-02-07 16:10:40 +0000

    More time for jupyter lab

--- src/jupyter_lab.sh
+++ src/jupyter_lab.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=Teach-LongJobs
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-04:00:00
 #SBATCH --mem=16GB
 #SBATCH --output=/home/%u/honours-project/contrastive-map/logs/jupyter.log
 
