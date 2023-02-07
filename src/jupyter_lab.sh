340c17e6940072a797bcaae7a950c3102bab7d0f
alv31415 <aleonvillares31415@gmail.com>
2023-02-07 15:53:20 +0000

    Added shell script to connect to jupyter lab

---
+++ src/jupyter_lab.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter
+#SBATCH --nodelist=landonia24
+#SBATCH --partition=Teach-LongJobs
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-02:00:00
+#SBATCH --mem=16GB
+#SBATCH --output=/home/%u/honours-project/contrastive-map/logs/jupyter.log
+
+STUDENT_ID=$(whoami)
+HOME_DIR=/home/${STUDENT_ID}/honours-project
+EXPERIMENT_DIR=${HOME_DIR}/contrastive-map/src/dev
+SCRATCH_DIR=/disk/scratch_big/${STUDENT_ID}
+SCRATCH_OUT_DIR=${SCRATCH_DIR}/output
+
+JUPYTER_DIR=${SCRATCH_OUT_DIR}
+
+source ${HOME_DIR}/henv/bin/activate
+
+cd ${JUPYTER_DIR}
+
+JUPYTER_PORT=8888
+
+echo "Running jupyter on $(pwd), at port ${JUPYTER_PORT}"
+
+jupyter lab --ip=0.0.0.0 --port=${JUPYTER_PORT}
