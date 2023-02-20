9b2849fd245cdd16a34f5915e6c343d09ab88eee
Antonio León Villares <55582768+alv31415@users.noreply.github.com>
2023-02-20 12:46:54 +0000

    fix run experiments

--- src/py/run_experiments.sh
+++ src/py/run_experiments.sh
@@ -25,7 +25,7 @@ echo "experiments.txt created succesfully"
 
 
 N_EXPERIMENTS=$(cat ${EXPERIMENT_FILE} | wc -l) 
-MAX_PARALLEL_JOBS=6
+MAX_PARALLEL_JOBS=8
 
 echo "${N_EXPERIMENTS} found. Running with maximum ${MAX_PARALLEL_JOBS} parallel jobs."
 
