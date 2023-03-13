526117feaacfa0eb005907192401ec18f1dec4fa
Antonio León Villares <55582768+alv31415@users.noreply.github.com>
2023-03-13 17:11:39 +0000

    update run scripts

--- src/py/batch_slurm_contrastive_run.sh
+++ src/py/batch_slurm_contrastive_run.sh
@@ -28,11 +28,37 @@ set -e
 echo "________________________________________"
 
 # define main directories
+
+if [ -z ${HOME_DIR} ]; then
+  HOME_DIR=/home/${STUDENT_ID}/honours-project
+fi
+
+if [ -z ${SCRATCH_DIR} ]; then
+  SCRATCH_DIR=/home/${STUDENT_ID}/honours-project
+fi
+
+if [ -z ${EXPERIMENT_DIR} ]; then
+  EXPERIMENT_DIR=${HOME_DIR}/contrastive-map/src/py
+fi
+
+if [ -z ${EXPERIMENT_FILE} ]; then
+  EXPERIMENT_FILE=${EXPERIMENT_DIR}/experiments.txt
+fi
+
+if [ -z ${EXPERIMENT_NAME} ]; then
+  EXPERIMENT_NAME=${SLURM_JOB_ID}
+fi
+
+DATA_DIR=${HOME_DIR}/contrastive-map/src/data/originals
+
+
+:'
 HOME_DIR=/home/${STUDENT_ID}/honours-project
 SCRATCH_DIR=/disk/scratch_big/${STUDENT_ID}
 EXPERIMENT_DIR=${HOME_DIR}/contrastive-map/src/py
 DATA_DIR=${HOME_DIR}/contrastive-map/src/data/originals
 EXPERIMENT_FILE=${EXPERIMENT_DIR}/experiments.txt
+'
 
 if [ -f "${EXPERIMENT_FILE}" ]; then
     echo "${EXPERIMENT_FILE} found."
@@ -53,16 +79,42 @@ echo "________________________________________"
 echo "Creating directory in scratch disk: ${SCRATCH_DIR}"
 mkdir -p ${SCRATCH_DIR}
 
+if [ -z ${SCRATCH_DATA_DIR} ]; then
+  SCRATCH_DATA_DIR=${SCRATCH_DIR}/data
+fi
+
+if [ -z ${SCRATCH_OUT_DIR} ]; then
+  SCRATCH_OUT_DIR=${SCRATCH_DIR}/output/${EXPERIMENT_NAME}
+fi
+
+EXPERIMENT_OUT_DIR=${EXPERIMENT_DIR}/output/${EXPERIMENT_NAME}
+SLURM_OUT_DIR=${EXPERIMENT_DIR}/slurm_logs
+
+:'
 SCRATCH_DATA_DIR=${SCRATCH_DIR}/data
 SCRATCH_OUT_DIR=${SCRATCH_DIR}/output
 
 EXPERIMENT_OUT_DIR=${EXPERIMENT_DIR}/output
 SLURM_OUT_DIR=${EXPERIMENT_DIR}/slurm_logs
+'
 
 mkdir -p ${SCRATCH_DATA_DIR}
 mkdir -p ${SCRATCH_OUT_DIR}
 mkdir -p ${EXPERIMENT_OUT_DIR}
 mkdir -p ${SLURM_OUT_DIR}
+
+if [[ "$DEBUG" == "true" ]]; then
+  echo "Debugging set to true"
+  echo "Home directory: ${HOME_DIR}"
+  echo "Scratch directory: ${SCRATCH_DIR}"
+  echo "Scratch data directory: ${SCRATCH_DATA_DIR}"
+  echo "Scratch output directory: ${SCRATCH_OUT_DIR}"
+  echo "Experiment directory: ${EXPERIMENT_DIR}"
+  echo "Experiment output directory: ${EXPERIMENT_OUT_DIR}"
+  echo "Data directory: ${DATA_DIR}"
+  echo "Experiment file: ${EXPERIMENT_FILE}"
+  echo "Experiment name: ${EXPERIMENT_NAME}"
+
 
 #rm -rf ${SCRATCH_DATA_DIR}/*.pk ||:
 
@@ -79,7 +131,7 @@ echo "Running main.py according to experiments from ${EXPERIMENT_FILE}"
 COMMAND="`sed \"${SLURM_ARRAY_TASK_ID}q;d\" ${EXPERIMENT_FILE}`"
 echo "Running provided command: ${COMMAND}"
 eval "${COMMAND}"
-echo "Command ran succesfully."
+echo "Command ran successfully."
 
 echo "________________________________________"
 
@@ -91,4 +143,4 @@ rsync --archive --update --compress --progress ${SCRATCH_OUT_DIR}/ ${EXPERIMENT_
 echo "________________________________________"
 
 END=$(date "+%d/%m/%Y %H:%M:%S")
-echo "Job completed succesfully at ${END}"
+echo "Job completed successfully at ${END}"
--- src/py/run_experiments.sh
+++ src/py/run_experiments.sh
@@ -2,13 +2,15 @@
 set -e
 
 # gather the files & directories to use
+DEBUG=false
 STUDENT_ID=$(whoami)
-EXPERIMENT_DIR=/home/${STUDENT_ID}/honours-project/contrastive-map/src/py
-
+HOME_DIR=/home/${STUDENT_ID}/honours-project
+EXPERIMENT_DIR=${HOME_DIR}/contrastive-map/src/py
+EXPERIMENT_NAME=final_run
 
 SCRATCH_DIR=/disk/scratch_big/${STUDENT_ID}
 SCRATCH_DATA_DIR=${SCRATCH_DIR}/data
-SCRATCH_OUT_DIR=${SCRATCH_DIR}/output
+SCRATCH_OUT_DIR=${SCRATCH_DIR}/output/${EXPERIMENT_NAME}
 
 MAIN_FILE=${EXPERIMENT_DIR}/main.py 
 SLURM_RUN_FILE=${EXPERIMENT_DIR}/batch_slurm_contrastive_run.sh
@@ -18,11 +20,10 @@ EXPERIMENT_FILE=${EXPERIMENT_DIR}/experiments.txt
 # generate experiments file
 echo "Generating experiments.txt"
 python ${GEN_EXPERIMENT_FILE} --main ${MAIN_FILE} \
-							  --scratch-out-dir ${SCRATCH_OUT_DIR} \
-							  --scratch-data-dir ${SCRATCH_DATA_DIR}
+                              --scratch-out-dir ${SCRATCH_OUT_DIR} \
+							                --scratch-data-dir ${SCRATCH_DATA_DIR}
 
-echo "experiments.txt created succesfully"
-
+echo "experiments.txt created successfully"
 
 N_EXPERIMENTS=$(cat ${EXPERIMENT_FILE} | wc -l) 
 MAX_PARALLEL_JOBS=8
@@ -31,4 +32,13 @@ echo "${N_EXPERIMENTS} found. Running with maximum ${MAX_PARALLEL_JOBS} parallel
 
 # run sbatch job
 echo "Running batch job: sbatch --array=1-${N_EXPERIMENTS}%${MAX_PARALLEL_JOBS} ${SLURM_RUN_FILE} ${EXPERIMENT_FILE}"
-sbatch --array=1-${N_EXPERIMENTS}%${MAX_PARALLEL_JOBS} ${SLURM_RUN_FILE}
+sbatch --array=1-${N_EXPERIMENTS}%${MAX_PARALLEL_JOBS}  \
+       --export=HOME_DIR=${HOME_DIR} \
+       --export=EXPERIMENT_DIR=${EXPERIMENT_DIR} \
+       --export=SCRATCH_DIR=${SCRATCH_DIR} \
+       --export=SCRATCH_DATA_DIR=${SCRATCH_DATA_DIR} \
+       --export=SCRATCH_OUT_DIR=${SCRATCH_OUT_DIR} \
+       --export=EXPERIMENT_NAME=${EXPERIMENT_NAME} \
+       --export=EXPERIMENT_FILE=${EXPERIMENT_FILE} \
+       --export=DEBUG=${DEBUG} \
+       ${SLURM_RUN_FILE}
